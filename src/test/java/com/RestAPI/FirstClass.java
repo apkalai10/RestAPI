47c9c154e93d5fac9f472a4efd907bcead214c59
Shobin@2023 <aravinthprasath3@gmail.com>
2024-04-14 23:33:06 +0530

    get,post,put,delete

--- src/test/java/com/RestAPI/FirstClass.java
+++ src/test/java/com/RestAPI/FirstClass.java
@@ -5,11 +5,13 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+
 class FirstClass {
 
+	int id;
 	
-	
-	@Test
+	@Test(priority = 0,enabled = true)
 	 void getUsers()
 	{
 		given()
@@ -22,5 +24,52 @@ class FirstClass {
 	}
 	
 	
+	@Test(priority = 1,enabled = true)
+	void createUser()
+	{
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("name", "aravinth");
+		map.put("job","tech");
+		id = given()
+		.contentType("application/json")
+		.body(map)
+		.when()
+		.post("https://reqres.in/api/users")
+		.jsonPath().getInt("id");
+		/*.then()
+		.statusCode(201)
+		.log().all();*/
+		
+	}
+	 
+	@Test(priority = 2,enabled = true,dependsOnMethods = {"createUser"})
+	void updateuser()
+	{
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("name", "aravinth");
+		map.put("job","gym");
+		given()
+		.contentType("application/json")
+		.body(map)
+		.when()
+		.put("https://reqres.in/api/users/"+id)
+		.then()
+		.statusCode(200)
+		.log().all();
+		
+		
+		
+	}
+	
+	@Test(priority = 3,enabled = true,dependsOnMethods = {"updateuser"})
+	void deleteuser()
+	{
+		given()
+		.when()
+		.delete("https://reqres.in/api/users/"+id)
+		.then()
+		.statusCode(204)
+		.log().all();
+	}
 	
 }
